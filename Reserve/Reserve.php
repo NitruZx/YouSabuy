127d5b9ea29d67a938d33a50b9b1033a603d416c
Natthapong <65070079@kmitl.ac.th>
2023-09-13 16:44:23 +0700

    test

--- Reserve/Reserve.php
+++ Reserve/Reserve.php
@@ -1,28 +1,34 @@
-<!-- <!DOCTYPE html>
-<html lang="en"
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
 
 </head>
 <body>
 <div id = "frm">  
-        <h1>Login</h1>  
+        <h1>Reserve Room</h1>  
         <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
-            <p>  
-                <label> UserName: </label>  
-                <input type = "text" id ="user" name  = "user" />  
+            <p>   
+                <lable>
+                   Name: </label>  
+                <input type = "text" id ="firstname" name  = "firstname"  placeholder="firstname"/>
+                <input type = "text" id ="lastname" name  = "lastname"  placeholder="lastname"/> 
             </p>  
-            <p>  
-                <label> lastname: </label>  
-                <input type = "password" id ="pass" name  = "pass" />  
-            </p>  
+            <p>
+                <label> Phone : </label>
+                <input type = "text" id ="phone" name = "phone" placeholder="phone"/>
+            </p>
+            <p>
+                <lable> Email : </lable>
+                <input type="text" id ="email" name="email" placeholder="email">
+            </p>
             <p>     
-                <input type =  "submit" id = "btn" value = "Login" />  
+                <input type =  "submit" id = "btn" value = "Reserve-Room" />  
             </p>  
         </form>  
     </div>  
 </body>
- -->
+
 
 <?php
     $f_name;
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
     echo "test";
-    echo "PLS do error"
+    echo "PLS do error";
+    echo "guy";
 ?>
