8d8b8b830899dec381288a5a186d2d7622c582d5
Kanitphong <gustsri1@gmail.com>
2023-09-13 15:53:33 +0700

    edit re

--- Reserve/Reserve.php
+++ Reserve/Reserve.php
@@ -1,9 +1,6 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en"
-<head>
-    <meta charset="UTF-8">
-
-</head>
+<head> </head>
 <body>
 <div id = "frm">  
         <h1>Login</h1>  
@@ -22,7 +19,7 @@
         </form>  
     </div>  
 </body>
- -->
+
 
 <?php
     $f_name;
@@ -30,6 +27,6 @@
     $Tel_number;
     $Email;
     $Person_Num;
-    
+    $check = 0;
 
 ?>
