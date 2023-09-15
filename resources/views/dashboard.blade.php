b7b94958afb2efeb38ee31230f4376399ecc3c20
Jessada Taengsuwan <65070041@kmitl.ac.th>
2023-09-16 02:03:41 +0700

    add Paymentservice-controller & edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Manage') }}
+        </h2>
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Payment') }}
+        </h2>
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Report') }}
         </h2>
     </x-slot>
 
