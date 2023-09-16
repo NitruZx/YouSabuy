5c06113bd1059c36230766ccddad583c982f1967
Jessada Taengsuwan <65070041@kmitl.ac.th>
2023-09-16 22:21:16 +0700

    add model

---
+++ database/migrations/2023_09_16_124321_create_rooms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('Type', 50);
+            $table->integer('Floor');
+            $table->string('Building', 10);
+            $table->string('status', 50);
+            $table->integer('Monthly_Price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
