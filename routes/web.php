dcf3ac48de6e342b2aca786dc12cbc8dbac5670f
Jessada Taengsuwan <65070041@kmitl.ac.th>
2023-09-19 15:54:30 +0700

    add middleware to view

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReserveController;
+use App\Http\Controllers\ReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +29,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/paymentlist', [PaymentController::class, 'show'])->name('payment');
+
 
 Route::get('/inform/report', function () {
     return view('/Client/Inform/report');
@@ -44,12 +45,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/paymentlist', [PaymentController::class, 'show'])->name('payment');
+    Route::get('reserve', [ReserveController::class, 'index']);
+    Route::post('/upload', 'App\Http\Controllers\FileController@upload')->name('upload.file');
+    Route::get('/inform', [ReportController::class, 'index'])->name('inform');
+    Route::get('/test', function () {
+        return view('/Client/test'); //Testing passing username variable (ไม่ต้องสนใจก็ได้)
+    });
 });
-Route::post('/upload', 'App\Http\Controllers\FileController@upload')->name('upload.file');
 
 
-Route::get('reserve', [ReserveController::class, 'index']);
 Route::post('addinfo', [ReserveController::class, 'addinfo']);
-
 
 require __DIR__.'/auth.php';
