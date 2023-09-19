2db408a688da5d549243dd90f655253ad462c1ef
Natthapong <65070079@kmitl.ac.th>
2023-09-19 18:25:45 +0700

    change front end welcumm page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\ReportController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('/roomdetail', function () {
