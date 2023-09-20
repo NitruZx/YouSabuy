22565ffa61752915aecd0dc9d78b48b64eb0ca33
Jessada Taengsuwan <65070041@kmitl.ac.th>
2023-09-20 23:32:39 +0700

    Revert "Repair_Request"
    This reverts commit f3701bad2695e5f6b88f6d938935bf75133c8860.

--- app/Http/Controllers/RepairRequestController.php
+++ app/Http/Controllers/RepairRequestController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Repair_Request;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -12,24 +11,5 @@ class RepairRequestController extends Controller
     public function index() {
         $reports = DB::table('repair_requests')->where('name', Auth::user()->name)->get();
         return view('Client/Inform/report', compact('reports'));
-    }
-    function addinfo(Request $request){
-        $request->validate([
-            'date' => 'required',
-            'name' => 'required',
-            'email' => 'required',
-            'room' => 'required',
-            'phone' => 'required|digits:10'    
-        ]);
-        $repair = new Repair_Request();
-        $repair->name = Auth::user()->name;
-        $repair->date = $request->input('date');
-        $repair->Room = $request->input('room');
-        $repair->tel = $request->input('phone');
-        $repair->description = $request->input('....');
-        $repair->receiver = $request->input('....');
-        $repair->status = $request->input('....');
-        $repair->save();
-        return redirect()->route('dashboard');
     }
 }
