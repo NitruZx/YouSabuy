8c254f6cbd25d1bad0970594f11536b4443b3cbb
Jessada Taengsuwan <65070041@kmitl.ac.th>
2023-10-07 21:17:33 +0700

    create status_card

--- app/Livewire/StatusCard.php
+++ app/Livewire/StatusCard.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\On; 
 use Livewire\Component;
 
 class StatusCard extends Component
 {
+    public $reg;
+
+    // public $status;
+
+    public function cancel($client_id) {
+        $affected = DB::table('registrations')
+              ->where('client_id', $client_id)
+              ->update(['reg_status' => 'cancelled']);
+        if ($affected) {
+            // $this->status = 'cancelled';
+            session()->flash('status', 'The registrations has been cancelled.');
+            return redirect(request()->header('Referer'));
+        }
+    }
+
+    public function resume($client_id) {
+        $affected = DB::table('registrations')
+              ->where('client_id', $client_id)
+              ->update(['reg_status' => 'accept']);
+        if ($affected) {
+            // $this->status = 'accept';
+            session()->flash('status', 'The registrations has been resumed.');
+            return redirect(request()->header('Referer'));
+        }
+    }
+
+    public function mount() {
+        $this->reg = DB::table('registrations')
+                    ->join('clients', 'clients.id', '=', 'registrations.client_id')
+                    ->where('client_id', Auth::user()->id)->first();
+        // $this->status = $this->reg->reg_status;
+    }
+
     public function render()
     {
         return view('livewire.status-card');
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import { Datepicker, Input, Collapse, Ripple, Carousel, initTE } from "tw-elements";
-initTE({ Datepicker, Carousel, Collapse, Ripple, Input });
+import { Datepicker, Input, Collapse, Ripple, Carousel, Alert, Modal, initTE } from "tw-elements";
+initTE({ Datepicker, Carousel, Collapse, Ripple, Alert, Modal, Input });
 // Initialization for ES Users
 
 window.Alpine = Alpine;
